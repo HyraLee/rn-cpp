c07efb3d4ecc25c50f8f97795a4db1620344e04e
HyraLee <140471957+HyraLee@users.noreply.github.com>
2023-08-21 11:23:20 +0700

    Http request: get

--- cpp-code/src/include/example.h
+++ cpp-code/src/include/example.h
@@ -1,11 +1,13 @@
 #ifndef EXAMPLE_H
 #define EXAMPLE_H
+#include "curl/curl.h"
 #include <string.h>
 #include <iostream>
 
 namespace example
 {
   long multiply(long a, long b);
+  size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userptr);
   std::string httpGet(std::string url);
 }
 
--- cpp-code/src/src/example.cpp
+++ cpp-code/src/src/example.cpp
@@ -7,8 +7,47 @@ namespace example
 		return a * b;
 	}
 
+	size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userptr)
+	{
+		((std::string *)userptr)->append((char *)contents, size * nmemb);
+		return size * nmemb;
+	}
+
 	std::string httpGet(std::string url)
 	{
-		return "Response!";
+		CURL *curl;
+		CURLcode res;
+
+		curl_global_init(CURL_GLOBAL_DEFAULT);
+
+		curl = curl_easy_init();
+		if (curl)
+		{
+			std::string response;
+
+			curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+			res = curl_easy_perform(curl);
+			if (res != CURLE_OK)
+			{
+				std::cerr << "curl_easy_perform() failed: "
+						  << curl_easy_strerror(res) << std::endl;
+			}
+			else
+			{
+				std::cout << "Response:\n"
+						  << response << std::endl;
+			}
+
+			curl_easy_cleanup(curl);
+			curl_global_cleanup();
+
+			return response;
+		}
+
+		curl_global_cleanup();
+		return "Response!!!";
 	}
 }
