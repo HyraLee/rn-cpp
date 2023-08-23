f9cc866acba20f3a26923d55d318e5ea83fd8cba
HyraLee <140471957+HyraLee@users.noreply.github.com>
2023-08-23 14:52:49 +0700

    Config cmake

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -18,5 +18,24 @@ Java_com_mariusreimer_rncppcode_RNCPPCodeModule_multiply(JNIEnv *env, jclass typ
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_mariusreimer_rncppcode_RNCPPCodeModule_httpGet(JNIEnv *env, jclass type, jstring url)
 {
-    return example::httpGet(url, "");
+    const char *urlStr = env->GetStringUTFChars(url, nullptr);
+    if (urlStr == nullptr)
+    {
+        // Handle the error, e.g., by returning an appropriate error string.
+        return env->NewStringUTF("Error converting jstring to const char*");
+    }
+
+    // Create a C++ std::string from the C-style string
+    std::string urlCpp(urlStr);
+
+    // Release the memory allocated for the C-style string
+    env->ReleaseStringUTFChars(url, urlStr);
+
+    // Now you can use urlCpp as a C++ std::string in your code
+
+    // Example usage:
+    std::string result = example::httpGet(urlCpp, "");
+
+    // Convert the C++ std::string back to a jstring for return
+    return env->NewStringUTF(result.c_str());
 }
